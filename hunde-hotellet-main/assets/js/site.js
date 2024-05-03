60a3355b1805581f677a61cb2b5d66eec9509e03
MikkelTMS <mtamsgaard@gmail.com>
2024-05-03 11:21:28 +0200

    Sidebar (wink wink)
    Det gik satme godt!

--- hunde-hotellet-main/assets/js/site.js
+++ hunde-hotellet-main/assets/js/site.js
@@ -1,2 +1,28 @@
-// write cool JS hwere!!
+const menuOpen = document.getElementById('menuOpen');
+const navcontainer = document.getElementById('navcontainer');
+const nav = document.getElementById('nav');
 
+
+// Attach click event listener
+menuOpen.addEventListener('click', function(){
+    // Check current display type
+    if (navcontainer.style.display === 'none') {
+        // If it's hidden, show it
+        navcontainer.style.display = 'block';
+    } else {
+        // If it's visible, hide it
+        navcontainer.style.display = 'none';
+    }
+
+    
+});
+
+
+nav.addEventListener('click', function() {
+
+    if (navcontainer.style.display === 'block'){ 
+        navcontainer.style.display = 'none';
+    }
+
+
+});
